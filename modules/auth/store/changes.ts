6e62d5ed7544fe5c5c46055c2676ccd432ab1123
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2022-07-17 05:41:21 +0300

    include logout in auth module
    - function for logging out included in auth module

--- modules/auth/store/changes.ts
+++ modules/auth/store/changes.ts
@@ -1,12 +1,27 @@
-const checkLoggedIn = ()=>{
-    if (typeof window !== 'undefined'){
+const empty = ''
+const checkLoggedIn:()=>boolean = ()=>{
+    if (rendered()){
         return localStorage.getItem('token')?true:false;
     }
 }
-const storeUserDetails=(token)=>{
-    if (typeof window !== 'undefined') {
+
+const rendered:()=>boolean = ()=>{
+    if (typeof window !== 'undefined'){
+        return true;
+    }
+    return false;
+}
+const getToken:()=>string = ()=>{
+    if (rendered()){
+        return localStorage.getItem('token');
+    }
+    return empty
+}
+const storeUserDetails:(token:string)=>string=(token:string)=>{
+    if (rendered()) {
         localStorage.setItem("token",token);
     }
+    return empty
 
 }
-export {checkLoggedIn,storeUserDetails}
+export {checkLoggedIn,storeUserDetails,getToken}
